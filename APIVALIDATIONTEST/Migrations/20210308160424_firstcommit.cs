098e85c5612d748e5d5b6a0c6948a194a966afc2
Bruno Rossi <bruno.gavioli@etec.sp.gov.br>
2021-03-08 13:24:17 -0300

    adcionando

---
+++ APIVALIDATIONTEST/Migrations/20210308160424_firstcommit.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIVALIDATIONTEST.Migrations
+{
+    public partial class firstcommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    PessoaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.PessoaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
